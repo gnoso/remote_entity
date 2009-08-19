bb53a286c5c149b4b00e534efa7a667c5cd6a579
Alan Johnson <alan@commondream.net>
2009-08-19 15:07:18 -0400

    Got includes working right.

--- lib/remote_entity.rb
+++ lib/remote_entity.rb
@@ -1,1 +1,5 @@
 require File.join(File.dirname(__FILE__), 'remote_entity', 'remote_entity')
+require File.join(File.dirname(__FILE__), 'remote_entity', 'entity_resource')
+require File.join(File.dirname(__FILE__), 'remote_entity', 'entity_record')
+require File.join(File.dirname(__FILE__), 'remote_entity',
+    'entity_record_associations')
